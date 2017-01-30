f3e7e0018b8f390b0a9260a70c34ff42ac613931
remeh <me@remy.io>
2017-01-30 21:35:23 +0100

    memos: get archived memos route.

--- api/memos/get.go
+++ api/memos/get.go
@@ -13,7 +13,26 @@ type Get struct {
 func (c Get) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	uid := api.ReadUser(r)
 
-	cs, err := memos.DAO().GetByUser(uid, memos.MemoActive)
+	// parse form
+	// ----------------------
+
+	r.ParseForm()
+	s := r.Form.Get("s")
+
+	memos.DAO()
+
+	state := memos.MemoActive
+	if s == "archived" {
+		state = memos.MemoArchived
+	}
+
+	// get the memos
+	// ----------------------
+
+	cs, err := memos.DAO().GetByUser(uid, state)
+
+	// render the response
+	// ----------------------
 
 	if err != nil {
 		api.RenderErrJson(w, err)
