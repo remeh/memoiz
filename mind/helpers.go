1eaa96230ccbf0184ab68ec677e9fc7eba9dfe24
remeh <remeh@remeh.fr>
2017-03-05 20:09:59 +0100

    mind: add some valid licenses.

--- mind/helpers.go
+++ mind/helpers.go
@@ -8,7 +8,10 @@ import (
 )
 
 var licenses = []string{
+	"CC BY-SA 4.0",
 	"CC BY-SA 3.0",
+	"CC BY-SA 2.0",
+	"CC BY-SA 2.5",
 	"http://creativecommons.org/licenses/by-sa/2.0",
 	"http://creativecommons.org/licenses/by-sa/2.5",
 	"http://creativecommons.org/licenses/by-sa/3.0",
